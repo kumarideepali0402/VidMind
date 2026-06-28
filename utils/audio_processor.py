c41c3abf3a609076ea4f3d99686897759eb9fd2f
Deepali <deepalikumari62004@gmail.com>
2026-06-28 17:22:45 +0530

    audio processing done

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -5,7 +5,7 @@ import os
 DOWNLOAD_DIR = 'downloads'
 os.makedirs(DOWNLOAD_DIR, exist_ok = True)
 
-def download_youtub_audio(url: str) -> str:
+def download_youtube_audio(url: str) -> str:
     output_path = os.path.join(DOWNLOAD_DIR,"%(title)s.%(ext)s");
     ydl_opts = {
         "format": "bestaudio/best",
@@ -25,7 +25,6 @@ def download_youtub_audio(url: str) -> str:
         filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
     return filename
 
-data= download_youtub_audio("https://www.youtube.com/watch?v=mtiOK2QG9Q0")
 
 
 
@@ -37,7 +36,7 @@ def convert_to_wav(input_path:str) -> str:
     audio.export(output_path, format="wav")
     return output_path
 
-data_final = convert_to_wav(data)
+
 
 
 def chunk_audio(wav_path : str, chunk_minutes : int = 10) -> list:
@@ -53,6 +52,24 @@ def chunk_audio(wav_path : str, chunk_minutes : int = 10) -> list:
         chunks.append(chunk_path)
     return chunks
 
-print(chunk_audio(data_final))
+
+def process_input(source : str) -> list:
+    if source.startswith("http://") or source.startswith("https://"):
+        print("It's a yt  url. Downloading audio..")
+        wav_path = download_youtube_audio(source)
+    else:
+        print("It's a local file. Converting to WAV...")
+        wav_path = convert_to_wav(source)
+    
+    print("Chunking audio...")
+    chunks = chunk_audio(wav_path)
+    print(f"Audio ready - {len(chunks)} chunk(s) created")
+    return chunks
+
+
+
+
+
+
 
 
